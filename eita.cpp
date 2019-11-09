5b46989b1a119bddd1f67a30558c400a63241ef1
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-09 11:52:59 -0300

    WIP

--- eita.cpp
+++ eita.cpp
@@ -2,26 +2,35 @@
 #include "verilated.h"
 #include <iostream>
 #include <fstream>
-#include <chrono>
 #include <thread>
+#include <ctime>
+#include <map>
+#include <functional>
+#include <string>
 
 int main(int argc, char** argv, char** env) {
     Verilated::commandArgs(argc, argv);
+
+    vluint8_t VProgramCounter::*pmd = &VProgramCounter::Clk;
+    //propmap props;
+    //props["name"] = &Foo::name;
+
     VProgramCounter* top = new VProgramCounter();
     long long last_pos = -1;
 
-    top->Reset = 0;
+    std::string::size_type sz;
+
+    top->Reset = std::stol("0", &sz);
+    printf("Reset: %d\n", top->Reset);
+    printf("Clk %d\n", top->*pmd);
 
     while (!Verilated::gotFinish()) {
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
-        std::ifstream myfile ("example.txt");
+        std::ifstream myfile ("jjj.txt");
         std::string line;
+
+        std::clock_t begin = clock();
         if (myfile.is_open())
         {
-            for(int i = 0; i <= last_pos; i++) {
-                getline (myfile,line);
-            }
             while (myfile.good())
             {
                 getline (myfile,line);
@@ -30,12 +39,19 @@ int main(int argc, char** argv, char** env) {
             }
             myfile.close();
         }
+        std::clock_t end = clock();
+        double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+        printf("\n\nSECS_1: %f\n\n", elapsed_secs);
 
         top->PCNext += 1;
         top->Clk += 1;
-        top->PCWrite = !top->PCWrite;
+        //top->PCWrite = !top->PCWrite;
+        begin = clock();
         top->eval();
-        printf("%i\n", top->PCResult);
+        end = clock();
+        elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+        printf("\n\nSECS_2: %f\n\n", elapsed_secs);
+        //printf("%i\n", top->PCResult);
     }
 
     delete top;
--- src/vv/core.clj
+++ src/vv/core.clj
@@ -1,5 +1,10 @@
-(ns vv.core)
+(ns vv.core
+  (:require [clojure.string :as str]))
 
 (spit "example.txt"
       "\njesus"
       :append true)
+
+(doseq [i (range 1)]
+  (spit "jjj.txt"
+        (str/join (range 3))))
