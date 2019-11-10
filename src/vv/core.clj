10c77beef1d7eaa6748f625cda44b84158ae2b1d
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-10 06:55:44 -0300

    WIP

--- src/vv/core.clj
+++ src/vv/core.clj
@@ -1,9 +1,6 @@
 (ns vv.core
-  (:require [clojure.string :as str]))
-
-(spit "example.txt"
-      "\njesus"
-      :append true)
+  (:require [clojure.string :as str]
+            [clojure.data :as data]))
 
 (defn command
   ([k]
@@ -25,14 +22,44 @@
   (spit "jjj.txt"
         "0:2"))
 
-(time
- (doseq [i (range 9421)]
-   (doseq [c [(command :clk 0)
-              (command :eval)
-              (command :pc-next i)
-              (command :pc-write 1)
-              (command :clk 1)
-              (command :eval)]]
-     (spit "caramba.txt"
-           (str c "\n")
-           :append true))))
+(def rise-m
+  {:pc-next (* i 2)
+   :pc-write 2})
+
+(def old-state
+  {:pc-next (* i 2)
+   :pc-write 1})
+
+
+
+(defn tick
+  ([i old-state rise-m]
+   (tick old-state i rise-m {}))
+  ([i old-state rise-m fall-m]
+   (vec
+    (concat
+     [(command :clk 0)
+      (command :eval)
+      (command :clk 1)]
+     (some->> (first (data/diff rise-m old-state))
+              (mapv (fn [[op arg]] (command op arg))))
+     [(command :eval)]))))
+
+(comment
+
+  (time
+   (doseq [i (range 10)]
+     (doseq [c (tick i {} {:pc-next (* i 1)
+                           :pc-write 1})]
+       (spit "caramba.txt"
+             (str c "\n")
+             :append true))))
+
+  #_[(command :clk 0)
+     (command :eval)
+     (command :clk 1)
+     (command :pc-next (* i 2))
+     (command :pc-write 1)
+     (command :eval)]
+
+  ())
