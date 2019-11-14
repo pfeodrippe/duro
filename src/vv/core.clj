314d6386bd301a1096dbc7ed01eb56ae8e128c1c
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-13 23:18:44 -0300

    JNR

--- src/vv/core.clj
+++ src/vv/core.clj
@@ -2,7 +2,48 @@
   (:require [clojure.string :as str]
             [clojure.data :as data]
             [vv.io]
-            [vv.parser]))
+            [vv.parser])
+  (:import
+   (jnr.ffi LibraryLoader)))
+
+(definterface LibUUID
+  (^int uuid_generate_time (^{:tag java.nio.ByteBuffer Out true} uuid_t)))
+
+(defn ^:private make-random-node
+  []
+  (let [bytes (byte-array 6)]
+    (.nextBytes (java.security.SecureRandom.) bytes)
+    (aset-byte bytes 5 (bit-or (aget bytes 5) 1))
+    bytes))
+
+(def libuuid
+  (.load (LibraryLoader/create LibUUID) "uuid"))
+
+(def node
+  (make-random-node))
+
+(def node-str
+  (apply format "%02x:%02x:%02x:%02x:%02x:%02x" node))
+
+(defn ^:private rewrite-node
+  [uuid-t]
+  (doseq [[ix byte] (map-indexed vector node)]
+    (.put uuid-t (+ 10 ix) byte))
+  uuid-t)
+
+(defn ^:private pack
+  [buffer]
+  (let [msb (.getLong buffer)
+        lsb (.getLong buffer)]
+    (java.util.UUID. msb lsb)))
+
+(comment
+
+  (time (let [uuid-t (java.nio.ByteBuffer/allocate 16)]
+     (.uuid_generate_time libuuid uuid-t)
+     (pack (rewrite-node uuid-t))))
+
+  ())
 
 ;; ALU
 (comment
@@ -26,12 +67,12 @@
      (every? (fn [{pc-result "ALUResult"
                    a "A"
                    b "B"}]
-                 (= pc-result (- a b)))
-               (doall
-                (for [i (range 200)]
-                  (let [input {"ALUControl" 2r0110
-                               "A" (* 2 i)
-                               "B" (* 4 i)}]
-                    (merge input (vv.io/eval file-based input))))))))
+               (= pc-result (- a b)))
+             (doall
+              (for [i (range 200)]
+                (let [input {"ALUControl" 2r0110
+                             "A" (* 2 i)
+                             "B" (* 4 i)}]
+                  (merge input (vv.io/eval file-based input))))))))
 
   ())
