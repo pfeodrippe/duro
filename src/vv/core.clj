9e48eaebbad3fb4e49856048c3bfeb64b8419f63
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-15 19:55:04 -0300

    Create JnrIO

--- src/vv/core.clj
+++ src/vv/core.clj
@@ -38,7 +38,38 @@
 ;; ALU
 (comment
 
+  ;; JNR
   (let [{:keys [:inputs :outputs]} (vv.parser/module-interface
+                                    "obj_dir/VALU32Bit.xml")
+        jnr-io (vv.io/jnr-io
+                {:request->out-id (->> inputs
+                                       (map-indexed
+                                        (fn [i input]
+                                          [input i]))
+                                       (into {}))
+                 :in-id->response (->> outputs
+                                       (map-indexed
+                                        (fn [i output]
+                                          [i output]))
+                                       (into {}))}
+                "/Users/feodrippe/dev/verilog-ex/obj_dir/libfob16.dylib")]
+    (time
+     (every? (fn [{pc-result "ALUResult"
+                   zero "Zero"
+                   a "A"
+                   b "B"}]
+               (let [expected-result (bit-or a b)]
+                 (and (= pc-result expected-result)
+                      (if (zero? expected-result) (= zero 1) (= zero 0)))))
+             (doall
+              (for [i (range 500)]
+                (let [input {"ALUControl" 2r0001
+                             "A" (* 2 i)
+                             "B" (- (* 4 i) 50)}]
+                  (merge input (vv.io/eval jnr-io input))))))))
+
+  ;; FILE
+  #_(let [{:keys [:inputs :outputs]} (vv.parser/module-interface
                                     "obj_dir/VALU32Bit.xml")
         file-based (vv.io/file-based-io
                     {:request-file "caramba.txt"
--- src/vv/io.clj
+++ src/vv/io.clj
@@ -1,5 +1,7 @@
 (ns vv.io
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:import
+   (jnr.ffi LibraryLoader Pointer)))
 
 (defprotocol VerilatorIO
   (eval [this input-data]))
@@ -47,3 +49,39 @@
   [params]
   (spit (:response-file params) "")
   (map->FileBasedIO params))
+
+(defrecord JnrIO
+    [native-lib top request->out-id in-id->response]
+    VerilatorIO
+    (eval [this input-data]
+      (try
+        (doseq [[op arg] input-data]
+          (.process_command native-lib top (request->out-id op) arg))
+        ;; eval
+        (.process_command native-lib top (count request->out-id) 0)
+        ;; read data
+        (->> in-id->response
+             (mapv (fn [[id attr]]
+                     [attr (.getInt (.read_module native-lib top) (* id 4))]))
+             (into {})))))
+
+(definterface NativeLibInterface
+  (^jnr.ffi.Pointer create_module [])
+  (^int process_command
+   [^jnr.ffi.Pointer top ^int command ^long command_value])
+  (^jnr.ffi.Pointer read_module [^jnr.ffi.Pointer top]))
+
+(defn jnr-io
+  [params lib-path]
+  (try (System/load lib-path)
+       (catch java.lang.UnsatisfiedLinkError _))
+  (let [lib-name (-> lib-path
+                     (str/split #"/")
+                     last
+                     (str/split #"\.")
+                     first
+                     (str/split #"lib")
+                     last)
+        native-lib (.load (LibraryLoader/create NativeLibInterface) lib-name)
+        top (.create_module native-lib)]
+    (map->JnrIO (assoc params :top top :native-lib native-lib))))
