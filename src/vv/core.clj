79ab03b55988824fb67dcee1522bece6e627fa42
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-15 19:56:40 -0300

    remove file based io

--- src/vv/core.clj
+++ src/vv/core.clj
@@ -2,43 +2,10 @@
   (:require [clojure.string :as str]
             [clojure.data :as data]
             [vv.io]
-            [vv.parser])
-  (:import
-   (jnr.ffi LibraryLoader Pointer Struct)))
-
-(definterface Eita
-  (^jnr.ffi.Pointer create_module [])
-  (^int process_command
-   [^jnr.ffi.Pointer top ^int command ^long command_value])
-  (^jnr.ffi.Pointer read_module [^jnr.ffi.Pointer top]))
-
-#_(System/load "/Users/feodrippe/dev/verilog-ex/obj_dir/libfob16.dylib")
-
-(def libeita
-  (.load (LibraryLoader/create Eita) "fob16"))
+            [vv.parser]))
 
 (comment
 
-  (def top (.create_module libeita))
-
-  (time
-   (->
-    (doto libeita
-      (.process_command top 0 2r0110)
-      (.process_command top 1 110)
-      (.process_command top 2 100))
-    (.process_command top 3 0)))
-
-  [(.getInt (.read_module libeita top) 0)
-   (.getInt (.read_module libeita top) 4)]
-
-
-  ())
-
-;; ALU
-(comment
-
-  ;; JNR
   (let [{:keys [:inputs :outputs]} (vv.parser/module-interface
                                     "obj_dir/VALU32Bit.xml")
         jnr-io (vv.io/jnr-io
@@ -52,7 +19,7 @@
                                         (fn [i output]
                                           [i output]))
                                        (into {}))}
-                "/Users/feodrippe/dev/verilog-ex/obj_dir/libfob16.dylib")]
+                "/Users/feodrippe/dev/verilog-ex/obj_dir/libfob17.dylib")]
     (time
      (every? (fn [{pc-result "ALUResult"
                    zero "Zero"
@@ -67,33 +34,5 @@
                              "A" (* 2 i)
                              "B" (- (* 4 i) 50)}]
                   (merge input (vv.io/eval jnr-io input))))))))
-
-  ;; FILE
-  #_(let [{:keys [:inputs :outputs]} (vv.parser/module-interface
-                                    "obj_dir/VALU32Bit.xml")
-        file-based (vv.io/file-based-io
-                    {:request-file "caramba.txt"
-                     :response-file "verilator-writer.txt"
-                     :request->out-id (->> inputs
-                                           (map-indexed
-                                            (fn [i input]
-                                              [input (str i)]))
-                                           (into {}))
-                     :in-id->response (->> outputs
-                                           (map-indexed
-                                            (fn [i output]
-                                              [(str i) output]))
-                                           (into {}))})]
-    (time
-     (every? (fn [{pc-result "ALUResult"
-                   a "A"
-                   b "B"}]
-               (= pc-result (- a b)))
-             (doall
-              (for [i (range 200)]
-                (let [input {"ALUControl" 2r0110
-                             "A" (* 2 i)
-                             "B" (* 4 i)}]
-                  (merge input (vv.io/eval file-based input))))))))
 
   ())
--- src/vv/io.clj
+++ src/vv/io.clj
@@ -6,50 +6,6 @@
 (defprotocol VerilatorIO
   (eval [this input-data]))
 
-(defn- parse-file-based-request
-  ([request->out-id k]
-   (parse-file-based-request request->out-id k 0))
-  ([request->out-id k v]
-   (if-let [op (request->out-id k)]
-     (let [cmd (str op ":" v)]
-       (str cmd
-            (str/join
-             (repeat (- 32 (count cmd)) " "))))
-     (throw (ex-info "`request->out-id` application to `k` is nil"
-                     {:request->out-id request->out-id
-                      :key k})))))
-
-(defn- parse-file-based-response
-  [in-id->response s]
-  (->> (str/split s #" ")
-       (mapv #(str/split % #":"))
-       (mapv (fn [[id v]]
-               [(in-id->response id)
-                (Integer/parseInt v)]))
-       (into {})))
-
-(defrecord FileBasedIO
-    [request-file response-file request->out-id in-id->response]
-    VerilatorIO
-    (eval [this input-data]
-      (try
-        (doseq [c (conj (mapv
-                         (fn [[op arg]]
-                           (parse-file-based-request request->out-id op arg))
-                         input-data)
-                        (parse-file-based-request
-                         {"Eval" (count request->out-id)}
-                         "Eval"))]
-          (spit request-file (str c "\n") :append true))
-        (while (empty? (slurp response-file)))
-        (parse-file-based-response in-id->response (slurp response-file))
-        (finally (spit response-file "")))))
-
-(defn file-based-io
-  [params]
-  (spit (:response-file params) "")
-  (map->FileBasedIO params))
-
 (defrecord JnrIO
     [native-lib top request->out-id in-id->response]
     VerilatorIO
