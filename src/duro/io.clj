5513d9b780280780c8f9cf7092dc9de8c35a1629
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-21 23:03:15 -0300

    Better test

--- src/duro/io.clj
+++ src/duro/io.clj
@@ -8,6 +8,7 @@
    (jnr.ffi LibraryLoader Pointer)))
 
 (defprotocol VerilatorIO
+  (input [this input-data])
   (eval [this input-data])
   (set-local-signal [this sig arg])
   (get-local-signal [this sig]))
@@ -22,30 +23,30 @@
   (^int get_local_signal [^jnr.ffi.Pointer top ^int sig])
   (^int eval [^jnr.ffi.Pointer top]))
 
-(defrecord JnrIO
-    [native-lib top input-ptr output-ptr
-     eval-flags-ptr request->out-id in-id->response]
+(defrecord JnrIO [native-lib top input-ptr output-ptr
+                  eval-flags-ptr request->out-id in-id->response]
   VerilatorIO
-    (eval [_this input-data]
-      (try
-        (doseq [[op arg] input-data]
-          (p :put-int
-             (.putInt ^jnr.ffi.Pointer input-ptr (* (request->out-id op) 4) arg)))
-        (p :eval
-           (.putInt ^jnr.ffi.Pointer eval-flags-ptr 0 1))
-        ;; read data
-        (p :parse-out
-           (p :waiting (while (not= (.getInt ^jnr.ffi.Pointer eval-flags-ptr 0) 0)))
-           (reduce (fn [acc v]
-                     (assoc acc
-                            (val v)
-                            (.getInt ^jnr.ffi.Pointer output-ptr (* (key v) 4))))
-                   {}
-                   in-id->response))))
-    (set-local-signal [_this sig arg]
-      (p :set-local-signal (.set_local_signal native-lib top sig arg)))
-    (get-local-signal [_this sig]
-      (p :get-local-signal (.get_local_signal native-lib top sig))))
+  (input [_this input-data]
+    (doseq [[op arg] input-data]
+      (p :put-int
+         (.putInt ^jnr.ffi.Pointer input-ptr (* (request->out-id op) 4) arg))))
+  (eval [this input-data]
+    (input this input-data)
+    (p :eval
+       (.putInt ^jnr.ffi.Pointer eval-flags-ptr 0 1))
+    ;; read data
+    (p :parse-out
+       (p :waiting (while (not= (.getInt ^jnr.ffi.Pointer eval-flags-ptr 0) 0)))
+       (reduce (fn [acc v]
+                 (assoc acc
+                        (val v)
+                        (.getInt ^jnr.ffi.Pointer output-ptr (* (key v) 4))))
+               {}
+               in-id->response)))
+  (set-local-signal [_this sig arg]
+    (p :set-local-signal (.set_local_signal native-lib top sig arg)))
+  (get-local-signal [_this sig]
+    (p :get-local-signal (.get_local_signal native-lib top sig))))
 
 (defn jnr-io
   [params lib-path]
--- test/duro/core_test.clj
+++ test/duro/core_test.clj
@@ -5,33 +5,66 @@
    [duro.io]
    [duro.verilator :as verilator]))
 
-(defn- tick
-  [top input]
-  (doto top
-    (duro.io/eval {})
-    (duro.io/eval (assoc input :div.i/i_clk 1)))
-  (duro.io/eval top {:div.i/i_clk 0}))
-
-(defn- reset
+(defn- ticker
   [top]
-  (tick top {:div.i/i_reset 1}))
+  (fn tick
+    ([] (tick {}))
+    ([data]
+     (doto top
+       (duro.io/eval {})
+       (duro.io/eval (assoc data :div.i/i_clk 1)))
+     (duro.io/eval top {:div.i/i_clk 0}))))
+
+(defn- resetter
+  [top]
+  (fn []
+    (tick top {:div.i/i_reset 1})))
+
+(defn- inputter
+  [top]
+  (fn [data]
+    (duro.io/input top data)))
 
 (deftest zipcpu-div-test
   (with-module top "zipcpu/rtl/core/div.v" {}
-    (doto top
-      (tick {:div.i/i_clk 0})
-      (reset)
-      (tick {:div.i/i_reset 0})
-      (tick {:div.i/i_wr 1
-             :div.i/i_signed 0
-             :div.i/i_numerator 33
-             :div.i/i_denominator 3}))
-    (loop [output (tick top {:div.i/i_wr 0
-                             :div.i/i_signed 0
-                             :div.i/i_numerator 0
-                             :div.i/i_denominator 0})
-           i 0]
-      (cond
-        (>= i 32) (throw (ex-info "Errrr!!" {:i i :output output}))
-        (zero? (:div.o/o_valid output)) (recur (tick top {}) (inc i))
-        :else output))))
+    (let [[tick reset input]
+          ((juxt ticker resetter inputter) top)]
+      (letfn [(init []
+                (tick {:div.i/i_clk 0})
+                (reset))
+              (request-div [n d signed?]
+                (let [out (tick {:div.i/i_reset 0
+                                 :div.i/i_wr 1
+                                 :div.i/i_signed (if signed? 1 0)
+                                 :div.i/i_numerator n
+                                 :div.i/i_denominator d})]
+                  (testing "o_busy should be `1` after div request"
+                    (is (= (:div.o/o_busy out) 1)))
+                  (testing "o_valid should be `0` after div request"
+                    (is (= (:div.o/o_valid out) 0))))
+                (input {:div.i/i_wr 0
+                        :div.i/i_signed 0
+                        :div.i/i_numerator 0
+                        :div.i/i_denominator 0}))
+              (div-result []
+                (loop [out (tick)
+                       i 0]
+                  (cond
+                    (> i 31)
+                    (throw (ex-info "div took longer than 32 cycles, this should never happen"
+                                    {:i i :out out}))
+
+                    (zero? (:div.o/o_valid out))
+                    (do (testing "o_busy should be `1` while a valid result does not appears"
+                          (is (= (:div.o/o_busy out) 1)))
+                        (recur (tick) (inc i)))
+
+                    :else out)))]
+        (init)
+        (request-div 36 3 true)
+        (let [out (div-result)]
+          (testing "correct quotient"
+            (is (= (:div.o/o_quotient out)
+                   (/ 36 3))))
+          (testing "after div result, o_busy should be `0`"
+            (is (zero? (:div.o/o_busy out)))))))))
