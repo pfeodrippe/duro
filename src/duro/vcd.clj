bb94495cc729f72faa9c4bdbe715f1e8f9b5ac95
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-24 22:22:41 -0300

    vc

--- src/duro/vcd.clj
+++ src/duro/vcd.clj
@@ -38,27 +38,34 @@
   (str body "\n$enddefinitions $end\n"))
 
 (defn gen-timeline
-  [wire->id-code wire-values]
+  [wire-info wire-values]
   (->> (mapcat (fn [[ts logic-values]]
                  [(str "#" ts)
                   (->> (mapv (fn [[k v]]
                                (str
                                 "b" (Integer/toBinaryString v)
-                                " " (wire->id-code k)))
+                                " " (:id-code (wire-info k))))
                              logic-values)
                        (str/join "\n"))])
                wire-values)
        (str/join "\n")))
 
-(comment
-
-  (let [{:keys [:date :version :comment :time-scale :module-name]}
-        {:date "abx"
-         :version "123"
-         :comment "Juju"
-         :time-scale "1ps"
-         :module-name "top"}
-
+(defn create-vcd-file
+  [file-path wire-values]
+  (let [date (str (new java.util.Date))
+        version "Duro dump."
+        comment "Generated automatically from duro simulation."
+        time-scale "1ps"
+        module-name "top"
+        wire-info {:data {:bit-size 2
+                          :id-code \s}
+                   :dado {:bit-size 1
+                          :id-code \a}}
+        wire-values [[12 {:data 2r11
+                          :dado 1}]
+                     [24 {:data 2r00
+                          :dado 0}]
+                     [30 {:dado 1}]]
         header (->> (concat
                      (gen-date date)
                      (gen-version version)
@@ -66,18 +73,43 @@
                      (gen-time-scale time-scale)
                      (gen-scope
                       module-name
-                      [(gen-var :wire 2 \s :data)
-                       (gen-var :wire 1 \a :dado)]))
+                      (mapv (fn [[wire {:keys [:bit-size :id-code]}]]
+                              (gen-var :wire bit-size id-code wire))
+                            wire-info)))
                     (str/join "\n")
                     (gen-definitions))
+        timeline (gen-timeline wire-info wire-values)]
+    (spit file-path (str header timeline))))
 
-        timeline (gen-timeline {:data \s
-                                :dado \a}
-                               [[12 {:data 2r11
-                                     :dado 1}]
-                                [24 {:data 2r00
-                                     :dado 0}]
-                                [30 {:dado 1}]])]
+(comment
+
+  (let [date (str (new java.util.Date))
+        version "Duro dump."
+        comment "Generated automatically from duro simulation."
+        time-scale "1ps"
+        module-name "top"
+        wire-info {:data {:bit-size 2
+                          :id-code \s}
+                   :dado {:bit-size 1
+                          :id-code \a}}
+        wire-values [[12 {:data 2r11
+                          :dado 1}]
+                     [24 {:data 2r00
+                          :dado 0}]
+                     [30 {:dado 1}]]
+        header (->> (concat
+                     (gen-date date)
+                     (gen-version version)
+                     (gen-comment comment)
+                     (gen-time-scale time-scale)
+                     (gen-scope
+                      module-name
+                      (mapv (fn [[wire {:keys [:bit-size :id-code]}]]
+                              (gen-var :wire bit-size id-code wire))
+                            wire-info)))
+                    (str/join "\n")
+                    (gen-definitions))
+        timeline (gen-timeline wire-info wire-values)]
     (spit "uba3.vcd" (str header timeline)))
 
   ())
