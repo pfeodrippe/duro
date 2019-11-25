a6691de39d90b58fe44c734a79c9b2e0d7bc3188
Paulo Feodrippe <pfeodrippe@gmail.com>
2019-11-24 21:44:19 -0300

    Vcs

--- src/duro/vcd.clj
+++ src/duro/vcd.clj
@@ -7,50 +7,72 @@
 
 (defn gen-date
   [body]
-  (section "$date" [body]))
+  (section "$date" body))
 
 (defn gen-version
   [body]
-  (section "$version" [body]))
+  (section "$version" body))
 
 (defn gen-comment
   [body]
-  (section "$comment" [body]))
+  (section "$comment" body))
 
 (defn gen-time-scale
   [body]
-  (section "$timescale" [body]))
+  (section "$timescale" body))
 
 (defn gen-var
   [type bit-size id-code reference]
-  (section
-   "$var"
-   [(name type) bit-size id-code (name reference)]))
+  (format "$var %s %d %c %s $end"
+          (name type) bit-size id-code (name reference)))
 
 (defn gen-scope
   [module-name body]
-  ["$scope" "module" module-name "end"
+  (concat
+   [(format "$scope module %s $end" module-name)]
    body
-   "$upscope $end"])
+   ["$upscope $end"]))
 
-(defn gen-definitios
+(defn gen-definitions
   [body]
-  [body "$enddefinitions"])
+  (str body "\n$enddefinitions $end\n"))
 
 (comment
 
-  (let [{:keys [:date :version :comment :time-scale]}
+  (let [{:keys [:date :version :comment :time-scale :module-name]}
         {:date "abx"
          :version "123"
          :comment "Juju"
-         :time-scale "1ps"}]
-    (->> (concat
-          (gen-date date)
-          (gen-version version)
-          (gen-comment comment)
-          (gen-time-scale time-scale))
-         (str/join "\n")
-         print))
-  []
+         :time-scale "1ps"
+         :module-name "top"}
+
+        header (->> (concat
+                     (gen-date date)
+                     (gen-version version)
+                     (gen-comment comment)
+                     (gen-time-scale time-scale)
+                     (gen-scope
+                      module-name
+                      [(gen-var :wire 2 \s :data)
+                       (gen-var :wire 1 \a :dado)]))
+                    (str/join "\n")
+                    (gen-definitions))
+
+        timeline (->> (mapcat (fn [[ts logic-values]]
+                                [(str "#" ts)
+                                 (->> (mapv (fn [[k v]]
+                                              (str
+                                               "b" (Integer/toBinaryString v)
+                                               " " ({:data \s
+                                                     :dado \a} k)))
+                                            logic-values)
+                                      (str/join "\n"))])
+                              [[12 {:data 2r11
+                                    :dado 1}]
+                               [24 {:data 2r00
+                                    :dado 0}]
+                               [30 {:dado 1}]])
+                      (str/join "\n"))]
+    (spit "uba2.vcd" (str header timeline)))
 
   ())
