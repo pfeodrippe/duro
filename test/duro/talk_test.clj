82be2aabbe8cf75d97dcb2a624b7fba67e9475ba
Paulo Feodrippe <pfeodrippe@gmail.com>
2020-01-13 23:59:16 -0300

    comb

--- test/duro/talk_test.clj
+++ test/duro/talk_test.clj
@@ -6,8 +6,11 @@
    [duro.vcd]
    [duro.verilator :as verilator]
 
+   [clojure.math.combinatorics :as combo]
    [com.billpiel.sayid.core :as sd]
-   [com.billpiel.sayid.trace :as tr]))
+   [com.billpiel.sayid.trace :as tr]
+   [clojure.string :as str]
+   [clojure.set :as set]))
 
 (comment
 
@@ -30,3 +33,79 @@
       (duro.io/eval top {:nao.i/x 1})
       (duro.core/dump-values top 20)
       (duro.core/dump-values top 30))))
+
+
+(comment
+  ;; 2
+  (def x "gnwj gfw eaddagf ewjuzsflk sfv ugmflafy")
+
+  (char (+ 97 (mod (- (+ 97 5) 97) 26)))
+
+  (int \ )
+
+  (mapv
+   (fn [i]
+     (->> x
+          (mapv int)
+          (mapv #(if (not= % 32)
+                   (+ (mod (- (- % i) 97) 26) 97)
+                   32))
+          (mapv char)
+          (str/join "")))
+   (range 30))
+
+  ())
+
+(comment
+  ;; 3
+  (def x
+    [[:1 [:head :iphone :ps4 :apple-tv :roku]]
+     [:2 [:iphone :switch :airpods]]
+     [:3 [:iphone :switch :airpods]]
+     [:4 [:iphone :airpods]]
+     [:5 [:head :ps4 :switch :chromecast :roku]]
+     [:6 [:iphone :switch :chromecast :airpods]]
+     [:7 [:head :ps4 :airpods :roku :chromecast]]])
+
+  (def items [:head :iphone :ps4 :apple-tv :roku])
+
+  (->> (for [y (combo/permutations x)]
+         (reduce (fn [{:keys [:dist :acc]} [p items]]
+                   (if-let [item
+                            (some #(when (not (contains? acc (last %)))
+                                     %)
+                                  (map-indexed vector items))]
+                     {:dist (assoc dist p item)
+                      :acc (conj acc (last item))}
+                     {:dist dist
+                      :acc acc}))
+                 {:dist {}
+                  :acc #{}}
+                 y))
+       (remove nil?)
+       (mapv :dist)
+       (filter #(= (count %) 7))
+       (sort-by #(->> %
+                      (mapv val)
+                      (mapv first)
+                      (reduce + 0)))
+       (take 10))
+
+  #_(->> (reduce (fn [{:keys [:dist :acc]} [p items]]
+                 (let [item
+                       (some #(when (not (contains? acc (last %)))
+                                %)
+                             (map-indexed vector items))]
+                   {:dist (assoc dist p item)
+                    :acc (conj acc (last item))}))
+               {:dist {}
+                :acc #{}}
+               x)
+       :dist
+       (mapv val)
+       (mapv first)
+       (reduce + 0))
+
+  (def acc #{:head})
+
+  ())
